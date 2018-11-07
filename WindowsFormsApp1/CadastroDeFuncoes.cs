78f1dd97c519569cace4d39e1715052ce3746722
David Christian <david9108@hotmail.com>
2018-11-07 01:27:09 -0200

    Refatorando tela de cadastro de novas funcoes do usuario

---
+++ WindowsFormsApp1/CadastroDeFuncoes.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WindowsFormsApp1
+{
+    internal class CadastroDeFuncoes
+    {
+        public static List<FuncoesDoUsuario> listTemp;
+
+        public CadastroDeFuncoes()
+        {
+            listTemp = new List<FuncoesDoUsuario>();
+            CarregarFuncoesCadastradas();
+        }
+
+        private void CarregarFuncoesCadastradas()
+        {
+            OpcoesAjuste ajuste;
+            FuncoesDoUsuario funcao;
+
+            foreach (FuncoesDoUsuario funcaoTemp in MicroOndas.Instance.listFuncoesUsuario)
+            {
+                ajuste = new OpcoesAjuste(funcaoTemp.tempo, funcaoTemp.potencia);
+                funcao = new FuncoesDoUsuario(ajuste, funcaoTemp.nome, funcaoTemp.caracter);
+
+                listTemp.Add(funcao);
+            }
+        }
+
+        public static List<FuncoesDoUsuario> CarregarArqFuncoes(String caminhoArq)
+        {
+            System.IO.StreamReader sReader;
+            OpcoesAjuste ajuste;
+            FuncoesDoUsuario funcao;
+            String[] lines;
+            string line;
+
+            listTemp = new List<FuncoesDoUsuario>();
+            sReader = new System.IO.StreamReader(caminhoArq);
+
+            try
+            {
+                while ((line = sReader.ReadLine()) != null)
+                {
+                    lines = line.Split(',');
+                    ajuste = new OpcoesAjuste(Convert.ToInt32(lines[2]), Convert.ToInt32(lines[3]));
+                    funcao = new FuncoesDoUsuario(ajuste, lines[0], lines[1][0]);
+
+                    listTemp.Add(funcao);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            sReader.Close();
+
+            return listTemp;
+        }
+
+        public static void SalvarModificacoes()
+        {
+            MicroOndas.Instance.listFuncoesUsuario = CadastroDeFuncoes.listTemp;
+        }
+    }
+}
--- WindowsFormsApp1/FrmCadastroFuncoes.cs
+++ WindowsFormsApp1/FrmCadastroFuncoes.cs
@@ -1,33 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp1
 {
     public partial class FrmCadastroFuncoes : Form
     {
-        private List<FuncoesDoUsuario> listTemp = new List<FuncoesDoUsuario>();
-
         public FrmCadastroFuncoes()
         {
             InitializeComponent();
-            CarregarFuncoesCadastradas();
+            new CadastroDeFuncoes();
+            VincularListAoDataSource();
         }
 
-        private void CarregarFuncoesCadastradas()
+        private void VincularListAoDataSource()
         {
-            foreach(FuncoesDoUsuario funcao in MicroOndas.Instance.listFuncoesUsuario)
-            {
-                listTemp.Add(new FuncoesDoUsuario(new OpcoesAjuste(funcao.tempo, funcao.potencia), funcao.nome, funcao.caracter));
-            }
-
-            funcoesDoUsuarioBindingSource.DataSource = listTemp;
+            funcoesDoUsuarioBindingSource.DataSource = CadastroDeFuncoes.listTemp;
         }
 
         private void btnCancelar_Click(object sender, EventArgs e)
@@ -37,37 +24,22 @@ namespace WindowsFormsApp1
 
         private void btnSalvar_Click(object sender, EventArgs e)
         {
-            MicroOndas.Instance.listFuncoesUsuario = listTemp;
+            CadastroDeFuncoes.SalvarModificacoes();
             Close();
         }
 
         private void abrirToolStripButton_Click(object sender, EventArgs e)
         {
-            System.IO.StreamReader sr;
-            String[] elements;
-            string line;
-
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                listTemp = new List<FuncoesDoUsuario>();
-                sr = new System.IO.StreamReader(openFileDialog1.FileName);
-
                 try
                 {
-                    while ((line = sr.ReadLine()) != null)
-                    {
-                        elements = line.Split(',');
-                        listTemp.Add(new FuncoesDoUsuario(new OpcoesAjuste(Convert.ToInt32(elements[2]), Convert.ToInt32(elements[3])), elements[0], elements[1][0]));
-                    }
+                    funcoesDoUsuarioBindingSource.DataSource = CadastroDeFuncoes.CarregarArqFuncoes(openFileDialog1.FileName);
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     MessageBox.Show(ex.Message);
                 }
-
-                sr.Close();
-
-                funcoesDoUsuarioBindingSource.DataSource = listTemp;
             }
         }
     }
